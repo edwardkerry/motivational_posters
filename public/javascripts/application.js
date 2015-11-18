2d0c57827c969622b0ab850cfc254659c15d5900
Edward Kerry <edwardkerry@gmail.com>
2015-11-18 19:32:41 +0000

    Step 5 initial picture loading

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,15 @@
-$(document).on('submit', '#search-form', function(e){
+$(document).on('submit', '#search-form', function(e) {
   e.preventDefault();
   var options = {
       keywords: $('#search-term').val(),
       container: $('#search-results')
   };
   findImagesOnGoogle(options); 
-    
+});
+
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $("#workspace").append(img);
 });
