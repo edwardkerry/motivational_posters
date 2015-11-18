89f70983dc0305c21aad5a0732155350593a8043
Edward Kerry <edwardkerry@gmail.com>
2015-11-18 20:19:01 +0000

    Step 7 complete

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,3 +17,32 @@ $(document).on('click', '#search-results img', function() {
 $(document).on('input', '#caption-text', function(){
   $("#caption").text($(this).val());
 });
+
+$(document).on('change', '#caption-left', function(){
+  $("#caption").css("left", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-top', function(){
+  $("#caption").css("top", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-width', function (){
+  $("#caption").css("width", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-size', function(){
+  $("#caption").css("font-size", $(this).val() + 'px');
+});
+
+$(document).on('change', '#caption-colour', function(){
+  $("#caption").css("color", $(this).val());
+});
+
+$(document).on('change', '#caption-align', function(){
+  $("#caption").css("text-align", $(this).val());
+});
+
+
+
+
+
