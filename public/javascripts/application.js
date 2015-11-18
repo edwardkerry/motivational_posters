86702b8a3629942c2d6ada73e44d2730ee76f56b
Edward Kerry <edwardkerry@gmail.com>
2015-11-18 12:14:32 +0000

    Step 5 complete

---
+++ public/javascripts/application.js
@@ -0,0 +1,9 @@
+$(document).on('submit', '#search-form', function(e){
+  e.preventDefault();
+  var options = {
+      keywords: $('#search-term').val(),
+      container: $('#search-results')
+  };
+  findImagesOnGoogle(options); 
+    
+});
